1c30045289d36de4a92a1d7a5f0f418b0940f185
Sree30111982 <39805760+Sree30111982@users.noreply.github.com>
2025-10-16 23:56:54 +0530

    Update main.tf

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -1,21 +1,5 @@
-# The configuration for the `remote` backend.
-#     terraform {
-#       backend "remote" {
-#         # The name of your Terraform Cloud organization.
-#         organization = "example-organization"
-#
-#         # The name of the Terraform Cloud workspace to store Terraform state files in.
-#         workspaces {
-#           name = "example-workspace"
-#         }
-#       }
-#     }
-#
-#     # An example resource that does nothing.
-#     resource "null_resource" "example" {
-#       triggers = {
-#         value = "A example resource that does nothing!"
-#       }
-#     }
-#
-#
+    resource "null_resource" "example" {
+      provisioner "local-exec" {
+        command = "echo 'Terraform test successful!'"
+      }
+    }
